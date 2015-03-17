7742b48ed991a69789b45d70ee7f3b9df445067e
Whalen <whalendillon@gmail.com>
2015-03-17 08:05:39 -0600

    Code added for checking large positive DBH changes after correcting as many of the negative DBH changes as I legitimately could. There are still some major outliers.

--- analysis/explore_data.R
+++ analysis/explore_data.R
@@ -212,7 +212,7 @@ stems %>% select(plot, cluster, tag, species, delta_dbh, year, year_dbh1, status
 # I changed values for 6 of 18 stems
 
 
-#### Create a plot-level data frame ####
+#### Create a plot-level data frame of infection status ####
 library(plyr)
 library(dplyr)
 
@@ -232,6 +232,181 @@ filter(plots, infected_bay_ct == 0)
 
 write.csv(plots, "analysis/data/plots_umca_infection.csv")
 # I sent this file to Francesco for informing the spread model
+
+#### Checking large positive DBH changes ####
+## So at this point I think I have corrected all the negative dbh change values that I legitimately could in the database. I have gone back and exported the query again and use this file below, recycling code from the beginning of this document.
+stems <- read.csv("analysis/data/Stem_Summary_negDBH_corrected_201402.csv")
+summary(stems)
+
+stems$Date <- as.character(stems$Date)
+class(stems$Date)
+library(lubridate)
+stems$year <- year(as.Date(stems$Date, format = "%m/%d/%Y"))
+summary(stems)
+library(plyr)
+library(dplyr)
+stems_sub <- filter(stems, is.na(year))
+stems_sub2 <- filter(stems, PlotID == "SECRET02", SpeciesID == "QUAG", year == 2012)
+
+stems$year[is.na(stems$year)] <- 2012
+summary(stems)
+stems[stems == -9999] <- NA
+summary(stems)
+
+library(dplyr)
+stems <- rename(stems, plot = PlotID)
+stems <- rename(stems, cluster = ClusterID, tag = TagNumber, 
+                species = SpeciesID, status = StemStatus, 
+                alive_class = AliveClass, dead_class= DeadClass,
+                slc = SympLeafCount, canker = CankerPresent, dbh = DBH,
+                lide_lf_symp = LideFoliarSymp, sod_dead = SOD_Dead, 
+                location = Location)
+str(stems)
+stems <- rename(stems, notes = StemSymptomNotes)
+summary(stems)
+
+
+#### Subset DBH data to create a data frame of remeasured stems: Round 2 ####
+dbh2014 <- stems %>% filter(dbh>0, year == 2014)
+length(unique(dbh2014$tag))
+# 4158, indicating there are 294 stems with no dbh measured in 2014
+
+dbh2003 <- stems %>% select(plot, tag, dbh, year) %>% 
+      filter(dbh>0, year == 2003) # these 2 are the same as in 2005
+# 11 observations
+
+dbh2004 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2004)
+# 3109 observations
+anti_join(dbh2003, dbh2004, by = "tag")
+dbh_a <- union(dbh2004, dbh2003)
+
+dbh2005 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2005)
+# 704 observations
+anti_join(dbh2005, dbh_a, by = "tag") # this indicates that the dbh data for 2004 and 2005 are unique
+dbh_a <- union(dbh_a, anti_join(dbh2005, dbh_a, by = "tag"))
+
+dbh2006 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2006)
+anti_join(dbh2006, dbh_a, by = "tag") # 2004, 2005, 2006 are independent sets
+dbh_a <- union(dbh_a, anti_join(dbh2006, dbh_a, by = "tag"))
+
+dbh2007 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2007)
+anti_join(dbh2007, dbh_a, by = "tag") # there is one tag with remeasured dbh in 2007
+dbh_a <- union(dbh_a, anti_join(dbh2007, dbh_a, by = "tag"))
+length(unique(dbh_a$tag)) # check for repeated tags, if obs > value, then there is a duplicate
+
+dbh2008 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2008)
+anti_join(dbh2008, dbh_a, by = "tag")
+dbh_a <- union(dbh_a, anti_join(dbh2008, dbh_a, by = "tag"))
+
+dbh2009 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2009)
+anti_join(dbh2009, dbh_a, by = "tag")
+dbh_a <- union(dbh_a, anti_join(dbh2009, dbh_a, by = "tag"))
+
+dbh2010 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2010)
+anti_join(dbh2009, dbh_a, by = "tag") # no unique dbh values for 2010, so no new recruitment, or stems entering the study
+
+dbh2011 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2011)
+anti_join(dbh2011, dbh_a, by = "tag")
+dbh_a <- union(dbh_a, anti_join(dbh2011, dbh_a, by = "tag"))
+
+dbh2012 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2012)
+anti_join(dbh2012, dbh_a, by = "tag")
+dbh_a <- union(dbh_a, anti_join(dbh2012, dbh_a, by = "tag"))
+
+dbh2014 <- stems %>% select(plot, tag, dbh, year) %>%
+      filter(dbh>0, year == 2014)
+anti_join(dbh2014, dbh_a, by = "tag") # this inidcates that there are 113 stems that were measured for the first time in 2014
+
+library(ggplot2)
+# Scatterplot of first dbh measurement (y-axis) against year
+qplot(year, dbh, data = dbh_a, geom="jitter")
+# Scatterplot of most recent dbh measurement
+qplot(year, dbh, data = dbh2014, geom = "jitter")
+
+# Data frame of stems that were measured or remeasured in 2014
+dbh <- right_join(dbh_a, dbh2014, by = c("plot","tag"))
+summary(dbh)
+
+dbh <- rename(dbh, dbh1 = dbh.x, dbh2 = dbh.y, year_dbh1 = year.x, year_dbh2 = year.y) # This data frame has all the stems for which we have an initial measurement AND a remeasurement in 2014
+dbh$delta_dbh <- dbh$dbh2 - dbh$dbh1
+write.csv(dbh, "analysis/data/dbh_remeasures.csv")
+summary(dbh)
+sum(dbh$delta_dbh, na.rm=T)
+# filter(dbh, delta_dbh < -20)
+
+
+#### Check the positive DBH change outliers ####
+
+library(ggplot2)
+qplot(dbh1, dbh2, data = dbh)
+qplot(delta_dbh, dbh2, data = dbh, color = year_dbh1)
+
+#Join dbh measure, remeasure, and change data to `stems` data frame
+stems <- left_join(stems, dbh, by = c("tag","plot"))
+str(stems)
+summary(stems)
+
+# Exploratory plot of dbh data
+qplot(delta_dbh, dbh1, data = stems %>% select(plot, cluster, tag, species, delta_dbh, dbh1, year, status) %>% 
+            group_by(plot, cluster, tag) %>%
+            filter(year == 2014, status == "Alive" | status == "Dead"), 
+      color = status)
+# A visual check shows that there are a handful of stems with growth > 20cm
+# I emailed Richard and Margaret about thresholds for where I should really look more closely at the measurements. Richard noted that there isn't much out there on growth rates for these species. Margaret suggested a few things, including other ways of looking at the data:
+# 1. "Have you tried looking at relative growth instead of the absolute increment growth?  Either DBH2/DBH1, or the instantaneous rate ln(DBH2/DBH1) / (time2-time1).  20 cm growth means a lot more to a 5 cm tree than a 50 cm tree… either way, it's still big.  I'd also consider this species by species, because I bet some grow much faster than others."
+stems$dbh2_dbh1 <- stems$dbh2/stems$dbh1
+stems$inst_grwth_rate <- log(stems$dbh2/stems$dbh1) / (stems$year_dbh2 - stems$year_dbh1)
+qplot(stems$dbh2_dbh1)
+
+qplot(dbh1, dbh2_dbh1, data = stems %>% select(plot, cluster, tag, species, dbh2_dbh1, dbh1, year, status) %>% 
+            group_by(plot, cluster, tag) %>%
+            filter(year == 2014, status == "Alive" | status == "Dead"), 
+      color = status)
+
+qplot(dbh1, inst_grwth_rate, data = stems %>% select(plot, cluster, tag, species, inst_grwth_rate, dbh1, year, status) %>% 
+            group_by(plot, cluster, tag) %>%
+            filter(year == 2014, status == "Alive" | status == "Dead"), 
+      color = status)
+
+qplot(dbh1, inst_grwth_rate, data = stems %>% select(plot, cluster, tag, species, inst_grwth_rate, dbh1, year_dbh1, year, status) %>% 
+            group_by(plot, cluster, tag) %>%
+            filter(species == "QUAG", year == 2014, status == "Dead"), 
+      color = status, main = "QUAG Instantaneous Growth Rate")
+
+
+stems %>% select(plot, cluster, tag, species, year, year_dbh1, status, delta_dbh, dbh1, dbh2, notes) %>% 
+      group_by(plot, cluster, tag) %>% 
+      filter(delta_dbh > 20, year == 2014, status == "Alive")
+# 8 stems, 
+stems %>% select(plot, tag, cluster, species, year, year_dbh1, status, dbh, delta_dbh, dbh1, dbh2, notes) %>% 
+      group_by(plot, cluster, tag) %>% 
+      filter(plot == "LAZY05", species == "QUKE", status == "Alive")
+
+stems %>% select(plot, tag, cluster, species, year, year_dbh1, status, dbh, delta_dbh, dbh1, dbh2, notes) %>% 
+      group_by(plot, cluster, tag) %>% 
+      filter(plot == "BOWES02", species == "UMCA", status == "Alive")
+
+stems %>% select(plot, cluster, tag, species, delta_dbh, dbh1, dbh2, year, year_dbh1, status) %>% group_by(plot, cluster, tag) %>% 
+      filter(between(delta_dbh, 10, 20), year == 2014, status == "Alive")
+# 39 stems
+
+stems %>% select(plot, cluster, tag, species, delta_dbh, year, year_dbh1, status) %>% group_by(plot, cluster, tag) %>% 
+      filter(between(delta_dbh, -10, -7), year == 2014, status == "Alive")
+# I changed values for 11 of 20 stems
+
+stems %>% select(plot, cluster, tag, species, delta_dbh, year, year_dbh1, status) %>% group_by(plot, cluster, tag) %>% 
+      filter(between(delta_dbh, -6.9, -5), year == 2014, status == "Alive")
+# I changed values for 6 of 18 stems
+
 
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### #
