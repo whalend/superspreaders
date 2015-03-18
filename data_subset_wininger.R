68a48334043837e5699719c1ce26348d4e30601e
Whalen <whalendillon@gmail.com>
2015-03-18 15:26:12 -0600

    Data subsets from Sonoma SOD project for Kerry Wininger. The 2014 stem level data for 12 plots, and hourly temperature data for the same 12 plots from 2003-2014.

--- data_subset_wininger.R
+++ data_subset_wininger.R
@@ -11,13 +11,17 @@ str(dbh2014)
 ### Filter stem data to subset of requested plots and year
 library(dplyr)
 
-kw_data <- stems %>%
+kw_stem_data <- stems %>%
       filter(plot %in% c("FOP04","FOP324","FOP333","FOP340","JROTH01","JROTH02","JROTH03","JROTH04","JROTH05","MROTH01","MROTH02","MROTH03"), year == 2014)
-str(kw_data)
-summary(kw_data)
+str(kw_stem_data)
+summary(kw_stem_data)
+write.csv(kw_stem_data, "deliverables/kw_stem_data.csv")
 
 ### "Plot temperature and precipitation points would be good too. Thanks for sending along some of this already." The temperature data is ready to go easily enough at the hourly level, but I still have some work to do on the precipitation data.
 
-
-
-
+### Read in the long format temperature data, it's a big file so might take a little longer than usual
+long_ppt <- readRDS("~/Documents/microclimate_tonini/Temperature/eof_pred_long.rds")
+str(long_ppt)
+kw_temp_data <- long_ppt %>% select(-posix_time) %>% filter(plotid %in% c("FOP04","FOP324","FOP333","FOP340","JROTH01","JROTH02","JROTH03","JROTH04","JROTH05","MROTH01","MROTH02","MROTH03"))
+summary(kw_temp_data)
+write.csv(kw_temp_data, "deliverables/kw_temp_data.csv")
