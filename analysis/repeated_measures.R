577303fc7d3609e661c5f4f9ce1064a81a3cc25c
Whalen <whalendillon@gmail.com>
2015-05-09 08:03:20 -0600

    Changed project settings (Rproj file) to "No" for restoring workspace and saving workspace. Changed some aesthetic formatting of the code, trying to improve readability. Deleted the results included in Sarah's code. Noted the assumptions of the linear mixed effects model and reason for using random effects. Build models with 1 or 2 random intercepts, and an interaction between bay density (number of stems) and year. But, this should probably be with plot b/c thats where the variation would be.

--- analysis/repeated_measures.R
+++ analysis/repeated_measures.R
@@ -1,7 +1,7 @@
 
 ## Repeated measures analysis ####
 
-#### Read in data for all stems that has been processed in "explore_data.R"
+## Read in data for all stems that has been processed in "explore_data.R"
 stems <- read.csv("analysis/data/stems_growth.csv")
 str(stems)
 summary(stems)
@@ -14,7 +14,7 @@ stems <- stems %>%
 stems$date <- as.Date(stems$date)
 stems$year <- as.factor(stems$year)
 
-### Create bay laurel only data frame ####
+#### Create bay laurel only data frame ####
 umca <- as.tbl(stems) %>%
       select(plot, cluster, tag, species, status, slc, dbh, delta_dbh, dbh2_1_ratio, year, date) %>%
       filter(species == "UMCA", status == "Alive" | status == "Dead")
@@ -35,46 +35,83 @@ filter(umca, plot == "YAHNG02")# Looks like leaf count probably not done/recorde
 
 #### Plot Level Repeated Measures Analysis Using Multi-level Mixed-Effects Regression ####
 
-## Linear Mixed Effects Method ####
-#### A couple of lines from Sarah's code to work from
+#### A couple of lines from Sarah's code to work from ####
+# lmer.null <- lmer(log(cum.slc + 1) ~ 1 + (1 | year))
+# lmer1 <- lmer(log(cum.slc + 1) ~ log(umca.ct + 1) + (1 | year))
 # anova(lmer.null, lmer1)
-# Models:
-# lmer.null: log(cum.slc + 1) ~ 1 + (1 | year)
-# lmer1: log(cum.slc + 1) ~ log(umca.ct + 1) + (1 | year)
-#          Df    AIC    BIC  logLik deviance  Chisq Chi Df Pr(>Chisq)    
-# lmer.null  3 4378.9 4394.0 -2186.4   4372.9                             
-# lmer1      4 3861.9 3882.1 -1927.0   3853.9 518.97      1  < 2.2e-16 ***
-#  ---
-#  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
-
 # lmer2 <- lmer(log(cum.slc + 1) ~ log(umca.ct + 1) + (1 + log(umca.ct+1)|year), na.action=na.omit)
 # anova(lmer1, lmer2)
-# Data: 
-#  Models:
-#  lmer1: log(cum.slc + 1) ~ log(umca.ct + 1) + (1 | year)
-#  lmer2: log(cum.slc + 1) ~ log(umca.ct + 1) + (1 + log(umca.ct + 1) | 
-#                                               lmer2:     year)
-#       Df    AIC    BIC  logLik deviance  Chisq Chi Df Pr(>Chisq)
-# lmer1  4 3861.9 3882.1 -1927.0   3853.9                         
-# lmer2  6 3865.2 3895.4 -1926.6   3853.2 0.7407      2     0.6905
 
-# Starting with plot level data b/c simpler w/fewer nested groups
-# Assumptions of linear mixed effects model:
-#     1. Gaussian (normal) distribution
-#     2. Errors ~ N(u, var)
+## Linear Mixed Effects Method ####
+
+## Assumptions of linear mixed effects model:
+##     1. b ~ N(0, D), random effects
+##     2. E ~ N(0, var), errors "white noise"
+##     3. These terms are independent in their distributions
+
+## The use of random effects prevent systematic variation between groups from ending up in the residual errors "white noise"
+
+## Starting with plot level data b/c simpler w/fewer nested groups
 library(lme4)
-#### Simplest models, with only one random effect
-m0 <- lmer(tot_slc ~ 1 + (1 | year), data = plot_umca)
-m0a <- lmer(tot_slc ~ 1 + (1 | year) + (1 | plot), data = plot_umca)
-m1 <- lmer(tot_slc ~ live_dens + (1 | year), data = plot_umca)
-m2 <- lmer(tot_slc ~ live_dens + (live_dens | year), data = plot_umca)
-anova(m0, m1, m2)
 
-m0 <- lmer(log1p(tot_slc) ~ 1 + (1 | year), data = plot_umca)
-m1 <- lmer(log1p(tot_slc) ~ log1p(live_dens) + (1 | year), data = plot_umca)
-m2 <- lmer(log1p(tot_slc) ~ log1p(live_dens) + (log1p(live_dens) | year), data = plot_umca)
-anova(m0, m1, m2) # Qualitatively similar results to SH's output
-anova(m1, m2)
+# Random Intercept Models
+## The random intercept model implies that there is one average curve (the population estimate) that may be shifted up or down by something normally distributed with variance d^2
+
+### Yi = Xi * B + Zi * bi + Ei
+###   bi ~ N(0, d^2), 
+###   Ei ~ N(0, covar)
+
+## The fitted lines for each group are parallel to the average curve
+
+## Simplest models, with only one random effect (random intercept)
+m0 <- lmer(tot_slc ~ 1 + (1 | year), data = plot_umca)
+## Add a second random effect where plot is nested within year
+m0a <- lmer(tot_slc ~ 1 + (1 | year) + (1 | plot), data = plot_umca)
+## One predictor + random effect (intercept)
+m1 <- lmer(tot_slc ~ live_dens + (1 | year), data = plot_umca)
+## One predictor + 2 random effects
+m1a <- lmer(tot_slc ~ live_dens + (1 | year) + (1 | plot), data = plot_umca)
+
+# Random slope, random intercept model
+## The random effects term in the random slope, random intercept model accounts for the interaction between the predictor variable(s) and group in addition to the systematic variation between groups.
+##    - similar to linear regresstion with nominal & continuous variables, but requires a lot of parameters to be estimated if there are many groups (1 for each)
+m2 <- lmer(tot_slc ~ live_dens + (1 + live_dens | year), data = plot_umca)
+## add the second random effect
+m2a <- lmer(tot_slc ~ live_dens + (1 + live_dens | year) + (1 | plot), data = plot_umca)
+
+## Compare all models using Chi-square test of ANOVA
+## Refits model(s) with maximum likelihood instead of REML
+anova(m0, m1, m2, m0a, m1a, m2a)
+## The random slope, random intercept model with the additional random effect is the best, but the high values for the metrics may indicate some misspecification. Also, this doesn't give me much information about the relationship between leaf count & the environment.
+
+# Plot the fitted models
+plot(m0)
+plot(m0a)
+plot(m1)
+plot(m1a)
+plot(m2)
+plot(m2a)
+## All of them show a lot of spread - expected because I know the distributions for leaf count and density aren't even close to normal
+par(mfrow=c(2,2))
+hist(plot_umca$tot_slc)
+hist(plot_umca$live_dens)
+## Log transformation improves the distributions, but with some zeroes for symptomatic leaf count I need to add a constant. The `log1p` function computes log(1+x) accurately for |x| much less than one (automatically adds the constant 
+log_tot_slc <- log1p(plot_umca$tot_slc); hist(log_tot_slc)
+log_live_dens <- log(plot_umca$live_dens); hist(log_live_dens)
+
+
+# Refit models with transformed variables
+m0 <- lmer(log_tot_slc ~ 1 + (1 | year), data = plot_umca)
+m0a <- lmer(log_tot_slc ~ 1 + (1 | year) + (1 | plot), data = plot_umca)
+m1 <- lmer(log_tot_slc ~ log_live_dens + (1 | year), data = plot_umca)
+m1a <- lmer(log_tot_slc ~ log_live_dens + (1 | year) + (1 | plot), data = plot_umca)
+m2 <- lmer(log_tot_slc ~ log_live_dens + (1 + log_live_dens | year), data = plot_umca)
+m2a <- lmer(log_tot_slc ~ log_live_dens + (1 + log_live_dens | year) + (1 | plot), data = plot_umca)
+
+anova(m0, m1, m2, m0a, m1a, m2a)
+anova(m1a, m2a)
+## The random slope, random intercept model with the plot random effect is the best, but only slightly better than the random intercept model with the year & plot random effects
+
 
 #### Print Model Coefficients
 coef(m0) # intercept only (null) model
