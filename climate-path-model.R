0a8ef9e9767975bd8e5555aff814adb37e6158df
Whalen <whalendillon@gmail.com>
2016-02-09 11:31:26 -0700

    Update data source for script.

--- climate-path-model.R
+++ climate-path-model.R
@@ -2,7 +2,7 @@
 #' I'm using the `piecewiseSEM package, which operationalized the methods from Shipley 2009 & 2013 in particular for fitting and assessing path models that enable applying hierarchical modeling methods.
 #' 
 #+ load data and packages ####
-load("pathmodel_data_20160112.RData")
+load("pathmodel_data.RData")
 library(plyr); library(dplyr); library(piecewiseSEM)
 library(nlme); library(lme4); library(lmerTest)
 
