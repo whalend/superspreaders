b56f8db0dd95c9075a22deec71475cb110834fbb
Whalen <whalendillon@gmail.com>
2016-02-23 11:48:41 -0700

    Fix missing plot data.
    Data for the 2014 plot visit to MROTH03 was missing from the data base query; I don't know why. I added it to the stem dataframes. This commit also includes some additional code cut from the "explore_data.R" script.

--- corrected-dbh.R
+++ corrected-dbh.R
@@ -67,7 +67,7 @@ summary(untagged_stems)
 ## Write out raw revised data sets ####
 write.csv(tagged_stems, "analysis/data/tagged_stems.csv", row.names = F)
 write.csv(untagged_stems, "analysis/data/untagged_stems.csv", row.names = F)
-write.csv(rbind(
+write.csv(all_stems <- rbind(
       select(tagged_stems, plotid, species, dbh, stem_status, date, year),
       untagged_stems),
       "analysis/data/all_stems.csv", row.names = F) 
@@ -188,11 +188,15 @@ tagged_stems <- as.tbl(left_join(tagged_stems,
                                  select(dbh_a, plotid, tag, dbh_entry),
                                  by = c("plotid", "tag")))
 summary(tagged_stems)
-summary(filter(tagged_stems, is.na(dbh_entry)))# NA are due to stems not meeting the 2.0 cm DBH requirement.
+summary(too_small_stems <- filter(tagged_stems, is.na(dbh_entry)))# NA are due to stems not meeting the 2.0 cm DBH requirement, except at least one curiosity for the initial_dbh
+filter(tagged_stems, is.na(dbh_entry), initial_dbh>2)
+# this is a plot that was abandoned very early in the study, may have been visited only one time
+# Take a closer look at these too small stems
+# fix(too_small_stems)
+# many are from abandoned plots, or are missing stems, and mostly did not meet DBH of 2.0cm upon first measurement
 
-# dropping stems that don't meet DBH threshold requirement
-tagged_stems <- filter(tagged_stems, dbh >= 2.0)
-write.csv(tagged_stems, "analysis/data/tagged-stems-corrected.csv")
+# dropping 194 stem records that don't meet DBH threshold requirement on their entry DBH measurement
+tagged_stems_corrected <- filter(tagged_stems, is.na(dbh_entry)==F)
 
 
 ## Explore DBH data: negative change in DBH ####
@@ -204,18 +208,87 @@ qplot(year, dbh_entry, data = dbh_a, geom="jitter", color = stem_status)
 # Dotplot of most recent dbh measurement
 qplot(stem_status, dbh, data = dbh2014, geom = "jitter", color = stem_status)
 
-# Data frame of tagged stems that were measured or remeasured in 2014
+
+
+## Match plots between establishment and 2014 remeasurement ####
 unique(dbh_establishment$year)
 dbh_establishment$year <- 2005
 unique(dbh2014$year)
 
-# Match plots between establishment and 2014 remeasurement
 length(unique(dbh_establishment$plotid))# 203 plots
 length(unique(dbh2014$plotid))# 195 plots
+dbh_establishment$plotid <- as.factor(dbh_establishment$plotid)
+tmp1 <- droplevels(unique(dbh_establishment$plotid))
+dbh2014$plotid <- as.factor(dbh2014$plotid)
+tmp2 <- droplevels(unique(dbh2014$plotid))
+setdiff(tmp1,tmp2)# identify plots not in 2014 data
+# These all make sense except for mroth03. For some reason, the 2014 plot visit is not being included in the Access database query.
+
+# Add the plot and stems, and update the DBH measurements according to the values in the database
+dbh2014 <- rbind(dbh2014, dbh2012 %>% filter(plotid == "mroth03"))
+# enter 2014 DBH values from database
+dbh2014$dbh[dbh2014$tag==1668] <- 68
+dbh2014$dbh[dbh2014$tag==1669] <- 49.2
+dbh2014$dbh[dbh2014$tag==1670] <- 56.1
+dbh2014$dbh[dbh2014$tag==1671] <- 51.2
+dbh2014$dbh[dbh2014$tag==1672] <- 66.9
+dbh2014$year <- 2014
+
+# These measurements for 2014 are then also missing from the tagged_stems dataframe
+filter(dbh2014, plotid == "mroth03")
+summary(filter(tagged_stems, plotid == "mroth03") %>% 
+              select(plotid, tag, date))
+names(tagged_stems)
+names(dbh2014)
+mroth_tmp1 <- filter(tagged_stems, plotid == "mroth03", year == 2011) %>% 
+      select(-plot_status, -species, -location, -dbh, -stem_status, -year)
+mroth_tmp <- filter(dbh2014, plotid == "mroth03")
+mroth_tmp <- left_join(mroth_tmp, mroth_tmp1)
+names(mroth_tmp)
+mroth_tmp <- select(mroth_tmp, plotid, plot_status, cluster, species, tag, location, dbh, initial_dbh, stem_status, sod_killed:date, year, dbh_entry)
+tagged_stems <- rbind(tagged_stems, mroth_tmp)
+tagged_stems_corrected <- rbind(tagged_stems_corrected, mroth_tmp)
+write.csv(tagged_stems_corrected, "analysis/data/tagged-stems-corrected.csv")
+
+
+# Reassess the plots not in 2014 data
 tmp1 <- droplevels(unique(dbh_establishment$plotid))
 tmp2 <- droplevels(unique(dbh2014$plotid))
+setdiff(tmp1,tmp2)
+# These make sense as plots that were not resampled in 2014 due to being lost for various resons earlier in the study. For assessing changes in basal area and abundance at the two time steps I want only the plots that were measured during both.
 
-dbh_changes <- rbind(dbh_establishment, dbh2014)
-summary(dbh_changes)
+# Remove plots from the establishment data set that were not revisited in 2014
+dbh_establishment <- filter(dbh_establishment, plotid != "bush01", 
+                            plotid != "mcneil01", plotid != "mcneil03", 
+                            plotid != "ponti01", plotid != "sumtv02", 
+                            plotid != "sweet01", plotid != "votru01")
+
+## Concatenate establishment and 2014 remeasurement dataframes ####
+tagged_dbh_changes <- rbind(dbh_establishment, dbh2014)
+summary(tagged_dbh_changes)
+tagged_dbh_changes <- select(tagged_dbh_changes, -plot_status)
+tagged_dbh_changes$species <- as.factor(tagged_dbh_changes$species)
+
+# export to data file
+write.csv(tagged_dbh_changes, "analysis/data/tag-dbh_0514-corrected.csv", row.names = F)
+
+# Compare Basal & Species Abundances ####
+par(mfrow=c(2,1))
+boxplot(dbh ~ year, 
+        data = dbhs %>% filter(species == "UMCA", status == "Alive"), 
+        main = "Live UMCA DBHs")
+boxplot(dbh ~ year, 
+        data = dbhs %>% filter(species == "UMCA", status == "Dead"), 
+        main = "Dead UMCA DBHs")
+
 qplot(factor(year), dbh, data = dbh_changes, geom = "jitter", color = stem_status, facets = .~species)
+
+basal_abund <- dbhs %>% 
+      #filter(status == "Alive") %>% 
+      group_by(plotid, species, year, status) %>% 
+      summarise(avg_ba_m2 = mean(pi*dbh^2/40000, na.rm = T), 
+                tot_ba_m2 = sum(pi*dbh^2/40000, na.rm = T),
+                abundance = length(dbh))
+summary(basal_abund)
+write.csv(basal_abund, "analysis/data/tag-dbh-plot.csv", row.names = F)
 
