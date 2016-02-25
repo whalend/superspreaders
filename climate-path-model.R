0051260680940a9fe95ccb0eff912a18b6246cd8
Whalen <whalendillon@gmail.com>
2016-02-25 10:44:03 -0700

    Update models with standardized variables.

--- climate-path-model.R
+++ climate-path-model.R
@@ -37,7 +37,7 @@ panel.hist <- function(x, ...)
 
 #+ examine oak plots data ####
 summary(oak_sod)
-oak_sod$plotid <- as.factor(oak_sod$plotid)
+# oak_sod$plotid <- as.factor(oak_sod$plotid)
 str(oak_sod)
 
 ## join wet-hours data
@@ -46,6 +46,10 @@ wet_hrs[is.na(wet_hrs)] <- 0# introduce a lot of zeroes for some columns
 oak_sod <- left_join(
       oak_sod, select(wet_hrs, -X), by = c("plotid","sample_year"))
 oak_sod$plotid <- as.factor(oak_sod$plotid)
+
+## drop 3d, PRISM, and regression rainfall variables based on discussion with Ryan; use a simple 2d spline interpolation or the Voronoi polygon values
+oak_sod <- select(oak_sod, -rain_tot_3d, -rain_tot_r, -rain_tot_psm)
+oak_sod <- filter(oak_sod, plotid != "ponti01", plotid != "sweet01", plotid != "bush01")
 
 #+ pairs plots of oak plots data ####
 pairs(na.omit(select(oak_sod, starts_with("rain"), ends_with("wet"))), 
@@ -57,11 +61,13 @@ pairs(na.omit(select(oak_sod, starts_with("rain"), ends_with("wet"))),
 # Total wet hours & hours below 10 & 14 are strongly correlated with rainy days.
 # Wet hours below 14 & total hours are correlated with total rainfall r = 0.66 - 0.76
 # Wet hours below 10 is correlated with total rainfall r = 0.61 - 0.68
+# To me, this indicates the possibility of using the wet-hours count combination variable of hours below 14 C in place of rainfall. This is only weakly correlated with the average temperature variables calculated for wet days. The number of hours below 14 C on wet days definitely captures the number of rainy days, as it should (r = 0.94).
 
-pairs(select(oak_sod, ends_with("rs"), ends_with("ds"), ends_with("wet")),
+pairs(na.omit(select(oak_sod, ends_with("rs"), ends_with("ds"), hrsblw14_wet, avgtmin_wet, avgtmax_wet)),
       lower.panel = panel.cor, diag.panel = panel.hist,
       main = "Oak Plots Temperature Variables")
 # Overall, correlations among variables are relatively weak with a few strong pairs. For specific pairs of variables it may be okay to use one from the dry season and one from the rainy season.
+
 pairs(select(oak_sod, ends_with("ds"), ends_with("wet")),
       lower.panel = panel.cor, diag.panel = panel.hist,
       main = "Oak Plots Temperature Variables")
@@ -78,7 +84,7 @@ pairs(select(oak_sod, ends_with("ds"), starts_with("rain")),
       main = "Oak Plots DS Temperature and Rainfall Variables")
 # Dry season temperature variables are only very weakly correlated with rainfall variables.
 
-pairs(na.omit(select(oak_sod, inf_oak_ct, tot_bay, tot_lfct, avg_lfct, H.2005, us.H.2005, hrsblw14_wet, hrsblw10_wet, hrs1020_wet, avgtmax_wet, avgtmin_wet, hrs_blw10rs, avg_tmax_rs, avg_tmin_rs, hrs_abv25ds, avg_tmax_ds, avg_tmin_ds, rain_tot_v, rain_tot_2d, twi15m)),
+pairs(na.omit(select(oak_sod, inf_oak_ct, tot_bay, tot_lfct, H.2005, H.2014, hrsblw14_wet, hrsblw10_wet, hrs1020_wet, avgtmax_wet, avgtmin_wet, hrs_blw10rs, avg_tmax_rs, avg_tmin_rs, hrs_abv25ds, avg_tmax_ds, avg_tmin_ds, rain_tot_v, rain_tot_2d, twi15m)),
       lower.panel = panel.cor, diag.panel = panel.hist)
 
 #' # Oak Plots Path Models
@@ -90,9 +96,15 @@ pairs(na.omit(select(oak_sod, inf_oak_ct, tot_bay, tot_lfct, avg_lfct, H.2005, H
 ## create binomial variable for oak infection
 # oak_sod$oak.inf <- cbind(oak_sod$inf_oak_ct, oak_sod$uninf_oak_ct)
 
-## subset data frame and transform/rescale variables
-oakplots.sub <- select(oak_sod, plotid:tot_lfct, candens15m, twi15m, elev_m, veght_m, hrs_14_20_rs:us_rich11, rain_tot_2d, rain_tot_v, rain_days_v, ends_with("wet"))
+## subset data frame and transform/rescale/standardize variables ####
+oakplots.sub <- select(oak_sod, plotid:tot_bay, tot_lfct, candens15m, twi15m, elev_m, veght_m, hrs_14_20_rs, avg_tmax_rs, avg_tmax_ds, H.2005, J.2005, os_rich05, us_rich05, us.E.2005, us.H.2005, rain_tot_2d, rain_tot_v, rain_days_v, hrsblw14_wet, avgtmax_wet, avgtmin_wet)
 summary(oakplots.sub)
+
+filter(oakplots.sub, is.na(elev_m))
+filter(oakplots.sub, is.na(avg_tmax_rs))
+filter(oakplots.sub, is.na(us_rich05))# one plot, never has infected oak
+filter(oakplots.sub, is.na(os_rich05))# this shouldn't have NAs b/c there should always be at least one species...
+
 pairs(select(oakplots.sub, -plotid), lower.panel = panel.cor, diag.panel = panel.hist)
 
 # binomial variable for infected/uninfected oaks in each plot
@@ -103,18 +115,19 @@ oakplots.sub$avg_lfct[is.na(oakplots.sub$avg_lfct)] <- 0
 
 oakplots.sub$rain_tot_v.cm <- oakplots.sub$rain_tot_v/10
 oakplots.sub$rain_tot_2d.cm <- oakplots.sub$rain_tot_2d/10
-oakplots.sub$rain_tot_3d.cm <- oakplots.sub$rain_tot_3d/10
+# oakplots.sub$rain_tot_3d.cm <- oakplots.sub$rain_tot_3d/10
 
 oakplots.sub$dys_14_20_rs <- oakplots.sub$hrs_14_20_rs/24
 oakplots.sub$dys_blw10_rs <- oakplots.sub$hrs_blw10rs/24
 oakplots.sub$dys_abv25_rs <- oakplots.sub$hrs_abv25rs/24
 # oakplots.sub$tot_lfct.10 <- oakplots.sub$tot_lfct/10
-# oakplots.sub$tot_lfct.100 <- oakplots.sub$tot_lfct/100
+oakplots.sub$tot_lfct.100 <- oakplots.sub$tot_lfct/100
 oakplots.sub$tot_lfct.log <- log1p(oakplots.sub$tot_lfct)
 oakplots.sub$tot_bay.log <- log1p(oakplots.sub$tot_bay)
-oakplots.sub$avg_lfct.log <- log1p(oakplots.sub$avg_lfct)
+# oakplots.sub$avg_lfct.log <- log1p(oakplots.sub$avg_lfct)
 # oakplots.sub$sample_year <- as.factor(oakplots.sub$sample_year)
 
+## rough path-model 1, how does this work ####
 oakplots.modlist <- list(
       lme(tot_bay.log ~ twi15m + H.2005, random = 1|sample_year/plotid, 
             data = oakplots.sub, na.action = na.omit),
@@ -245,18 +258,55 @@ sem.coefs(oakplots.modlist, oakplots.sub, standardize = 'scale')
 #' 
 #' For base GLM there are five types of residuals available, c("deviance", "pearson", "working","response", "partial")
 
-m1 <- lme(tot_bay.log ~ twi15m + H.2005, random = ~1|sample_year,
-          data = oakplots.sub, na.action = na.omit)
+
+# pairs plot of data for the model
+oakplots.sub2 <- select(oakplots.sub, plotid:inf_oak_ct, infected_bay_ct:twi15m, avg_tmax_ds:us.H.2005, rain_tot_v:oak.inf, tot_bay.log:tot_lfct.log)
+pairs(select(oakplots.sub2, -c(plotid:inf_oak_ct), -oak.inf),
+      lower.panel = panel.cor, diag.panel = panel.hist)
+
+# scale variables
+names(oakplots.sub2)
+## untransformed and scaled
+oakplots.sub2$inf_bay_ct.scl <- scale((oakplots.sub2$infected_bay_ct))
+oakplots.sub2$tot_bay_ct.scl <- scale((oakplots.sub2$tot_bay))
+oakplots.sub2$tot_slc.scl <- scale((oakplots.sub2$tot_lfct))
+oakplots.sub2$twi15m.scl <- scale((oakplots.sub2$twi15m))
+oakplots.sub2$avg_tmax_ds.scl <- scale(oakplots.sub2$avg_tmax_ds)
+oakplots.sub2$shannons2005.scl <- scale(oakplots.sub2$H.2005)
+oakplots.sub2$pielous2005.scl <- scale(oakplots.sub2$J.2005)
+oakplots.sub2$rain_tot.scl <- scale(oakplots.sub2$rain_tot_v)
+oakplots.sub2$rain_days.scl <- scale(oakplots.sub2$rain_days_v)
+oakplots.sub2$avgtmax_wet.scl <- scale(oakplots.sub2$avgtmax_wet)
+
+## transformed and scaled
+oakplots.sub2$inf_bay_ct.log.scl <- scale(log1p(oakplots.sub2$infected_bay_ct))
+oakplots.sub2$tot_bay_ct.log.scl <- scale(log1p(oakplots.sub2$tot_bay))
+oakplots.sub2$tot_slc.log.scl <- scale(log1p(oakplots.sub2$tot_lfct))
+oakplots.sub2$twi15m.log.scl <- scale(log1p(oakplots.sub2$twi15m))
+
+
+
+m1 <- lme(tot_bay_ct.log.scl ~ twi15m.log.scl + shannons2005.scl, 
+          random = ~1|sample_year,
+          data = oakplots.sub2, na.action = na.omit)
 summary(m1)
 plot(m1, main = "Total # Bay Laurel ~ TWI + Diveristy Residuals")
 
-m2 <- lme(H.2005 ~ twi15m, random = ~1|sample_year, data = oakplots.sub,
+m1a <- lme(tot_bay_ct.scl ~ twi15m.scl + shannons2005.scl, 
+          random = ~1|sample_year,
+          data = oakplots.sub2, na.action = na.omit)
+summary(m1a)
+plot(m1a, main = "Total # Bay Laurel ~ TWI Residuals")
+
+m2 <- lme(shannons2005.scl ~ twi15m.log.scl, 
+          random = ~1|sample_year, data = oakplots.sub2,
           na.action = na.omit)
 summary(m2)
 plot(m2, main = "Diversity ~ TWI Residuals")
 
-m3 <- lme(avg_tmax_ds ~ tot_bay.log + twi15m, random = ~1|sample_year,
-          data = oakplots.sub, na.action = na.omit)
+m3 <- lme(avg_tmax_ds.scl ~ tot_bay_ct.log.scl + twi15m.log.scl, 
+          random = ~1|sample_year,
+          data = oakplots.sub2, na.action = na.omit)
 summary(m3)
 plot(m3)
 # m3a <- lmer(avg_tmax_ds ~ tot_bay.log + twi15m + (1|sample_year),
@@ -264,68 +314,74 @@ plot(m3)
 # summary(m3a); AIC(m3a)
 # plot(m3a)
 
-m4 <- lme(tot_lfct.log ~ tot_bay.log + avg_tmax_ds + rain_tot_2d.cm + twi15m + H.2005, random = ~1|sample_year, data = oakplots.sub, 
+m4 <- lme(tot_slc.log.scl ~ tot_bay_ct.log.scl + avg_tmax_ds.scl + rain_days.scl + twi15m.log.scl + shannons2005.scl, 
+          random = ~1|sample_year, data = oakplots.sub2, 
           na.action = na.omit)
 plot(m4)
 summary(m4)
 E2 <- resid(m4, type = "normalized")# equivalent of "standardized" in plot
 # F2 <- fitted(m4)
 # plot(F2, E2)
-plot(E2 ~ tot_bay.log, data = oakplots.sub, ylab = "resids", xlab = "total # bay laurel")# this seems the most likely source of the pattern
-plot(E2 ~ avg_tmax_ds, data = oakplots.sub, ylab = "resids", xlab = "temp")
-plot(E2 ~ rain_tot_2d.cm, data = oakplots.sub, ylab = "resids", xlab = "rain")
-plot(E2 ~ twi15m, data = oakplots.sub, ylab = "resids", xlab = "twi")
-plot(E2 ~ H.2005, data = oakplots.sub, ylab = "resids", xlab = "Shannon's Index")
-boxplot(E2 ~ sample_year, data = oakplots.sub, ylab = "resids", xlab = "year")
+plot(E2 ~ tot_bay_ct.log.scl, data = oakplots.sub2, ylab = "resids", xlab = "total # bay laurel")# this seems the most likely source of the pattern
+plot(E2 ~ avg_tmax_ds.scl, data = oakplots.sub2, ylab = "resids", xlab = "temp")
+plot(E2 ~ rain_days.scl, data = oakplots.sub2, ylab = "resids", xlab = "rain")
+plot(E2 ~ twi15m.log.scl, data = oakplots.sub2, ylab = "resids", xlab = "twi")
+plot(E2 ~ shannons2005.scl, data = oakplots.sub2, ylab = "resids", xlab = "Shannon's Index")
+boxplot(E2 ~ sample_year, data = oakplots.sub2, ylab = "resids", xlab = "year")
 ## These plots indicate that the trend in the residuals is not a result of the predictors, at least none of them are contributing the substantial amount abserved.
 
-m4a <- lme(tot_lfct.log ~ tot_bay.log + avg_tmax_ds + rain_tot_2d.cm + twi15m + H.2005, random = ~1|sample_year/plotid, data = oakplots.sub, 
+m4a <- lme(tot_slc.log.scl ~ tot_bay_ct.log.scl + avg_tmax_ds.scl + rain_days.scl + twi15m.log.scl + shannons2005.scl, 
+           random = ~1|sample_year/plotid, data = oakplots.sub2, 
           na.action = na.omit)
 # I believe this is allowing the intercept to vary among groups within sample year
 plot(m4a)
 summary(m4a)
 
-m4b <- lmer(tot_lfct.log ~ tot_bay.log + avg_tmax_ds + rain_tot_2d.cm + twi15m + H.2005 + (1|sample_year/plotid), data = oakplots.sub)
+m4b <- lmer(tot_slc.log.scl ~ tot_bay_ct.log.scl + avg_tmax_ds.scl + rain_days.scl + twi15m.log.scl + shannons2005.scl + (1|sample_year) + (1|plotid), 
+            data = oakplots.sub2)
 # this formulation is allowing the intercept to vary among each group
 summary(m4b)
 plot(m4b)
 image(getME(m4b, name = "L"))
 image((getME(m4b, name = "A")))
 
-m5 <- glmer(oak.inf ~ tot_lfct.log + avg_tmax_ds + rain_tot_2d.cm + H.2005 + (1|sample_year),
-            data = oakplots.sub, family = binomial(link = "logit"), 
+m5 <- glmer(oak.inf ~ tot_slc.log.scl + avg_tmax_ds.scl + rain_days.scl + shannons2005.scl + (1|sample_year),
+            data = oakplots.sub2, family = binomial(link = "logit"), 
             na.action = na.omit)
 summary(m5)
 plot(m5)
-summary(oakplots.sub)
+summary(oakplots.sub2)
 # lfct: 0-8.8, tmax: 19.4-31.4, rain: 199.5-1529.1, H.2005: 0-1.6
 # based on these ranges I'm supposing that the rain variable is throwing things off re: large eigenvalue
 
-oakplots.sub$rain_tot_2d.dm <- oakplots.sub$rain_tot_2d.cm/10
-m5a <- glmer(oak.inf ~ tot_lfct.log + avg_tmax_ds + rain_tot_2d.dm + H.2005 + (1|sample_year),
-             data = oakplots.sub, family = binomial(link = "logit"), 
-             na.action = na.omit)
-# still fails to converge at set tolerance level, but avoids large eigenvalue
-summary(m5a)# essentially the same fixed effects values taking into account rainfall transformation
-plot(m5a)# same as for model m5
+## After standardizing (scale) all the variables I no longer get an error when running this model.
 
-# add an observation-level (plot) random effect to account for additional variation...
-m5b <- glmer(oak.inf ~ tot_lfct.log + avg_tmax_ds + rain_tot_2d.cm + H.2005 + (1|sample_year) + (1|plotid),
-             data = oakplots.sub, family = binomial(link = "logit"), 
-             na.action = na.omit)
-summary(m5b)# notable changes to estimates and p-values; lots of variance attributed to plot-level random effect
-plot(m5b)# this is...artsy? still with the nonlinear negative trend
 
-image(getME(m5a, name = "L"))
-image(getME(m5b, name = "L"))
+# oakplots.sub$rain_tot_2d.dm <- oakplots.sub$rain_tot_2d.cm/10
+# m5a <- glmer(oak.inf ~ tot_lfct.log + avg_tmax_ds + rain_tot_2d.dm + H.2005 + (1|sample_year),
+#              data = oakplots.sub, family = binomial(link = "logit"), 
+#              na.action = na.omit)
+# # still fails to converge at set tolerance level, but avoids large eigenvalue
+# summary(m5a)# essentially the same fixed effects values taking into account rainfall transformation
+# plot(m5a)# same as for model m5
+# 
+# # add an observation-level (plot) random effect to account for additional variation...
+# m5b <- glmer(oak.inf ~ tot_lfct.log + avg_tmax_ds + rain_tot_2d.cm + H.2005 + (1|sample_year) + (1|plotid),
+#              data = oakplots.sub, family = binomial(link = "logit"), 
+#              na.action = na.omit)
+# summary(m5b)# notable changes to estimates and p-values; lots of variance attributed to plot-level random effect
+# plot(m5b)# this is...artsy? still with the nonlinear negative trend
+# 
+# image(getME(m5a, name = "L"))
+# image(getME(m5b, name = "L"))
 
 
 # Fit Path Model
 oakplots.modlist <- list(m1,m2,m3,m4,m5)
 
-sem.fit(oakplots.modlist, oakplots.sub)
+sem.fit(oakplots.modlist, oakplots.sub2)
 sem.model.fits(oakplots.modlist)
-sem.coefs(oakplots.modlist, oakplots.sub)
+sem.coefs(oakplots.modlist, oakplots.sub2)
 sem.coefs(oakplots.modlist, oakplots.sub, standardize = 'scale')
 
 
