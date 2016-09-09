327ba45323fa285765ecbc12a528a83a496a362a
Whalen <whalendillon@gmail.com>
2016-09-09 11:39:09 -0600

    Comment out p-value reporting for pairs plots.

--- climate-path-model.R
+++ climate-path-model.R
@@ -18,11 +18,11 @@ panel.cor <- function(x, y, digits = 2, cex.cor, ...)
       text(0.5, 0.6, txt)
       
       # p-value calculation
-      p <- cor.test(x, y)$p.value
-      txt2 <- format(c(p, 0.123456789), digits = digits)[1]
-      txt2 <- paste("p= ", txt2, sep = "")
-      if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
-      text(0.5, 0.4, txt2)
+      # p <- cor.test(x, y)$p.value
+      # txt2 <- format(c(p, 0.123456789), digits = digits)[1]
+      # txt2 <- paste("p= ", txt2, sep = "")
+      # if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
+      # text(0.5, 0.4, txt2)
 }
 
 panel.hist <- function(x, ...)
